db80c62eb9e6bed7d7bb58f26ff8d30b08b76c38
CoulibalyMamadou <81160947+CoulibalyMamadou@users.noreply.github.com>
2021-07-19 06:02:52 +0200

    update commit : update previous quotation

--- src/api/program.service.js
+++ src/api/program.service.js
@@ -129,9 +129,16 @@ export const getProgramQuoteConstraintListFill = ({ programId }) => {
 	})
 }
 
+export const getProgramQuoteConstraintListFillOfOffice = ({ programId }) => {
+	// return fetch(`${API}/program/quoteConstraint/${programId}`, {
+	return fetch(`${API}/quoteConstraint/${programId}`, {
+		headers: loadHeaderWithAuth()
+	})
+}
+
 export const getAllProgramQuotationListFill = ({ programId }) => {
-	// return fetch(`${API}/program/quotation/${programId}`, {
-	return fetch(`${API}/quotation/${programId}`, {
+	return fetch(`${API}/program/quotation/${programId}`, {
+		// return fetch(`${API}/quotation/${programId}`, {
 		headers: loadHeaderWithAuth()
 	})
 }
